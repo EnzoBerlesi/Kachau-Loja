e4fd250f609f35d100e9e9ef3fed70063b9d0f76
jaumsw <j.calixtodegodoy@gmail.com>
2025-06-25 16:27:30 -0300

    + de uma origem, se der ruim, fodeu

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,10 +5,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://katchauloja.vercel.app', 
+    origin: [
+      'https://katchauloja.vercel.app',
+      'https://katchauloja-mqxlxm9zs-jaumsws-projects.vercel.app',
+      'https://katchauloja-jaumsws-projects.vercel.app/',
+      'https://katchauloja-jaumsws-projects.vercel.app/',
+    ], 
     credentials: true,
   });
 
   await app.listen(3000);
 }
 bootstrap();
+
