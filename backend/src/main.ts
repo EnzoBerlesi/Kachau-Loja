64417b922430a25adef3fee8cfa2568d6a7df1c7
jaumsw <j.calixtodegodoy@gmail.com>
2025-06-25 15:38:53 -0300

    mudança do nome + link vercel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://katchauloja.vercel.app', 
     credentials: true,
   });
 
